e26f7c9c8cdf9a2ce33af5e921bec59f7373b1e7
Dozgulbas <dozgulbas@anl.gov>
2023-01-24 17:45:03 -0600

    Accept camera name and number parameters

--- camera_module_client/launch/camera_publisher.launch.py
+++ camera_module_client/launch/camera_publisher.launch.py
@@ -1,17 +1,40 @@
+
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    launch_d = LaunchDescription()
+        
+    camera_name = LaunchConfiguration('camera_name')
+    camera_number = LaunchConfiguration('camera_number')
+
+    declare_use_camera_name_cmd = DeclareLaunchArgument(
+        naem = "camera_name",
+        default_value='CameraPublisher1',
+        description='Flag to accept camera name')
+
+    declare_use_camera_number_cmd = DeclareLaunchArgument(
+        name = 'camera_number',
+        default_value="0",
+        description='Flag to accept camera number')
 
     camera_module_client = Node(
             package = 'camera_module_client',
             namespace = 'std_ns',
             executable = 'camera_publisher',
             output = "screen",
-            name='CameraPublisher1'
-    )
+            name = camera_name,
+            parameters = [{"camera_number":camera_number}],
+            emulate_tty=True
 
+    )
+    launch_d = LaunchDescription()
+
+    launch_d.add_action(declare_use_camera_name_cmd)
+    launch_d.add_action(declare_use_camera_number_cmd)
     launch_d.add_action(camera_module_client)
     return launch_d
